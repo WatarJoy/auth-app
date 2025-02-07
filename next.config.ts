f6d1f3f3027c30bdc2d431f8a9d8b07ee42cfb9c
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2025-02-07 14:56:11 +0200

    path fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/auth_app',
+  basePath: '/auth-app',
   images: {
     unoptimized: true,
   },
